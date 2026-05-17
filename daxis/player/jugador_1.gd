20505657eb666dab3ff35794a9d1c034426f5813
RODRIGAMER669 <alexis.rr0501@gmail.com>
2026-05-17 11:44:32 +0200

    funcionalidad de tp cuando recibes daño en lava, tambien vacio hace daño

--- daxis/player/jugador_1.gd
+++ daxis/player/jugador_1.gd
@@ -9,7 +9,10 @@ extends CharacterBody2D
 @export var air_friction = 700
 
 var vida_actual = 100
+var invulnerable := false
+
 var checkpoint_pos = Vector2()
+var last_safe_position = Vector2()
 
 @onready var ani_player = $ani_j1
 @onready var vida_ui = $CanvasLayer/vida
@@ -19,10 +22,14 @@ var brain_full = preload("res://player/sprites/Health and Points Bars/Sprites/Br
 var brain_mid = preload("res://player/sprites/Health and Points Bars/Sprites/Brain Bar/Brain Stage 2.png")
 var brain_low = preload("res://player/sprites/Health and Points Bars/Sprites/Brain Bar/Brain Stage 3.png")
 
+
 func _ready() -> void:
 	add_to_group("jugadores")
 	actualizar_vida_ui()
+
 	checkpoint_pos = global_position
+	last_safe_position = global_position
+
 
 func actualizar_vida_ui():
 	if vida_actual > 66:
@@ -31,6 +38,7 @@ func actualizar_vida_ui():
 		vida_ui.texture = brain_mid
 	else:
 		vida_ui.texture = brain_low
+
 
 func update_animation(input_axis):
 	if not is_on_floor():
@@ -44,17 +52,24 @@ func update_animation(input_axis):
 		ani_player.speed_scale = 1
 		ani_player.play("idle")
 
+
 func _physics_process(delta: float) -> void:
 	var input_axis = Input.get_axis("mover_izq","mover_der")
+
 	apply_gravity(delta)
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	handle_jump()
 	handle_air_acceleration(input_axis, delta)
 	update_animation(input_axis)
+
 	comprobar_dano_tilemap()
+
+	if is_on_floor():
+		last_safe_position = global_position
+
 	move_and_slide()
-	
+
 
 
 func apply_gravity(delta):
@@ -72,47 +87,92 @@ func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
+
 func handle_jump():
 	if is_on_floor():
 		if Input.is_action_pressed("saltar"):
 			velocity.y = jump_force
 
+
 func handle_air_acceleration(input_axis, delta):
 	if is_on_floor(): return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, speed * input_axis, air_acceleration * delta)
-		
+
+
+
 func comprobar_dano_tilemap():
+
+	if invulnerable:
+		return
 
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
+
 		if collider.name == "peligros":
-			recibir_dano(34)
-				
+
+			var tilemap = collider
+			var tile_pos = tilemap.local_to_map(tilemap.to_local(collision.get_position()))
+			var tile_data = tilemap.get_cell_tile_data(0, tile_pos)
+
+			if tile_data == null:
+				continue
+
+			var damage = tile_data.get_custom_data("damage")
+
+			if damage == true:
+				recibir_dano(34)
+				break
+
+
+
 func recibir_dano(cantidad):
-	vida_actual -=cantidad	
+	if invulnerable:
+		return
+	vida_actual -= cantidad
 	if vida_actual < 0:
-		vida_actual = 0		
-	actualizar_vida_ui()	
+		vida_actual = 0
+
+	actualizar_vida_ui()
+
 	if vida_actual <= 0:
 		morir()
-		
+	else:
+		respawn_zona_segura()
+
+
 func morir():
+	invulnerable = true
 	set_physics_process(false)
-	
+
 	global_position = checkpoint_pos
 	velocity = Vector2.ZERO
-	
+
 	vida_actual = 100
 	actualizar_vida_ui()
-	
+
+	await get_tree().process_frame
+
 	set_physics_process(true)
-	
+	invulnerable = false
+
+
+func respawn_zona_segura():
+	invulnerable = true
+	set_physics_process(false)
+
+	global_position = last_safe_position
+	velocity = Vector2.ZERO
+
+	await get_tree().process_frame
+
+	set_physics_process(true)
+	invulnerable = false
 
 func mostrar_mensaje(texto: String):
 	mensaje_ui.text = texto
 	mensaje_ui.visible = true
-	
+
 	await get_tree().create_timer(2.0).timeout
 	mensaje_ui.visible = false
